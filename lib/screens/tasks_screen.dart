1a5ee7b3ea298c500f03128e882d6df8a174a3c9
Peter Harrison <blackriver@simiya.com>
2020-05-01 21:47:25 -0700

    Created background screen

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,6 +5,63 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+      ),
+      body: SafeArea(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding:
+                    EdgeInsets.only(left: 30, top: 60, right: 30, bottom: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    CircleAvatar(
+                      child: Icon(
+                        Icons.list,
+                        size: 30,
+                        color: Colors.lightBlueAccent,
+                      ),
+                      backgroundColor: Colors.white,
+                      radius: 30,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Todoey',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      '12 Tasks',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+      ),
     );
   }
 }
